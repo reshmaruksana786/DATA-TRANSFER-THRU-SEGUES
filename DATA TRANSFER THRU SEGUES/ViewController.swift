5e107a3ffc8f6faa0f11e9262a5a4d072aa3a40e
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-11-19 10:36:06 +0530

    Initial Commit

---
+++ DATA TRANSFER THRU SEGUES/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATA TRANSFER THRU SEGUES
+//
+//  Created by Syed.Reshma Ruksana on 19/11/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
